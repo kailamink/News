d4538a7a0d89c0efd4db1423f234e2372de37734
Kaila Minkowitz <kailamink@gmail.com>
2019-05-14 17:36:56 -0400

    Set up a config file + pom.xml + several models

---
+++ src/main/java/Model/Article.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Article
+{
+    private Source source = new Source();
+    private String author;
+    private String title;
+    private String description;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+    public Source getSource() {
+        return source;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ src/main/java/Model/Source.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Source {
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+}
